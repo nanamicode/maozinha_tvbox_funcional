766c3e995957c69163d4ac86afb719e3e6cab5fc
Vinicius Prado <elvinio2008@gmail.com>
2026-09-06 00:29:42 -0300

    feat: make 3D frontend fully localhost-bundleable

--- web/app.js
+++ web/app.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
+import * as THREE from '/vendor/three.module.js';
 
 const canvas=document.querySelector('#gl');
 const renderer=new THREE.WebGLRenderer({canvas,antialias:true,alpha:true});
